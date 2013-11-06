13479fc04f96423dc27aab7307f4a6c7bb8bc271
Jordan Scales <scalesjordan@gmail.com>
2013-11-05 21:56:40 -0500

    Added a pin to center on the user's current location

--- www/js/map.js
+++ www/js/map.js
@@ -4,13 +4,20 @@ function initialize() {
   navigator.geolocation.getCurrentPosition(function(data) {
     var latitude = data.coords.latitude;
     var longitude = data.coords.longitude;
+    var coords = new google.maps.LatLng(latitude, longitude);
 
     var mapOptions = {
       zoom: 8,
-      center: new google.maps.LatLng(latitude, longitude),
+      center: coords,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title: 'Hello World!'
+    });
   });
 }
